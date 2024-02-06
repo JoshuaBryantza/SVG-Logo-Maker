35d653bd70cf281e92437e016a0ef92040d04045
Joshua Bryant <joshuabryantza@gmail.com>
2024-02-05 17:54:12 -0800

    Made initial structure and tested it

---
+++ app.js
@@ -0,0 +1,41 @@
+var inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+  {
+    type: 'input',
+    message: 'Text',
+    name: 'imageText',
+  }
+];
+
+
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => createImage(answers))
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
+
+init();
+
+function createImage(data) {
+  const svgXml = `
+ <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+  <circle cx="150" cy="100" r="80" fill="green" />
+
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">${data.imageText}</text>
+
+</svg>
+  `;
+  fs.writeFile("logo.svg", svgXml, (err) =>
+    err ? console.error(err) : console.log('Success!')
+  );
+}
