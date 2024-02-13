be0d3e2c0f96ca134cc6466aa07838a7c8705f95
Joshua Bryant <joshuabryantza@gmail.com>
2024-02-12 16:53:48 -0800

    Added all the shapes and the prompt for the user to select the shape

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 
 const questions = [
   {
+    type: 'list',
+    message: 'Pick your shape',
+    choices: ['Circle', 'Triangle', 'Square'],
+    name: 'shapePick'
+  },
+  {
     type: 'input',
     message: 'Text',
     name: 'imageText',
@@ -23,7 +29,7 @@ const questions = [
 function init() {
   inquirer
     .prompt(questions)
-    .then((answers) => createImage(answers))
+    .then((blah) => createImage(blah))
     .catch((error) => {
       if (error.isTtyError) {
         // Prompt couldn't be rendered in the current environment
@@ -36,16 +42,34 @@ function init() {
 init();
 
 function createImage(data) {
-  const svgXml = `
- <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  var svgXml = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-  <circle cx="150" cy="100" r="80" fill="${data.shapeColor}" />
-
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.imageText}</text>
-
-</svg>
   `;
+  switch (data.shapePick) {
+    case "Circle":
+      svgXml += `<circle cx="150" cy="100" r="80" fill="${data.shapeColor}" />`;
+      break;
+    case "Triangle":
+      svgXml += `<polygon points="150, 18 244, 182 56, 182" fill="${data.shapeColor}" />`;
+      break;
+    case "Square":
+      svgXml += `<rect x="70" y="20" width="160" height="160" stroke="black" fill="${data.shapeColor}" stroke-width="5"/>`;
+      break;
+  }
+
+  svgXml += `  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.imageText}</text>
+
+  </svg>`;
+
   fs.writeFile("logo.svg", svgXml, (err) =>
     err ? console.error(err) : console.log('Success!')
   );
 }
+
+//Make an if statement that inserts the users shape input, this if statment should insert the correct code snippet for the shape picked  by the user
+
+ // if (data.shapePick == "Circle") {
+  //   svgXml += `<circle cx="150" cy="100" r="80" fill="${data.shapeColor}" />`;
+  // } else if (data.shapePick == "Triangle") {
+  //   svgXml += `<polygon points="150, 18 244, 182 56, 182" fill="${data.shapeColor}" />`;
+  // }
